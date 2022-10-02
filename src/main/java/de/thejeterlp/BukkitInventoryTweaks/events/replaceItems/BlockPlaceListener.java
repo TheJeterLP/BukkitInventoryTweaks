5dbaed44b38fae8e4585ee158689243addc000e0
TheJeterLP <joey.peter1998@gmail.com>
2022-10-02 14:43:24 +0200

    Fix shovel and hoe getting removed at right click

--- src/main/java/de/thejeterlp/BukkitInventoryTweaks/events/replaceItems/BlockPlaceListener.java
+++ src/main/java/de/thejeterlp/BukkitInventoryTweaks/events/replaceItems/BlockPlaceListener.java
@@ -18,7 +18,8 @@ public class BlockPlaceListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockPlace(BlockPlaceEvent e) {
         Player p = e.getPlayer();
-        if (e.isCancelled() || !Config.REPLACE_ITEMS_ON_BLOCK_PLACE.getBoolean() || !p.hasPermission("bit.replaceitems.blockplace")) return;
+        if (e.isCancelled() || !Config.REPLACE_ITEMS_ON_BLOCK_PLACE.getBoolean() || !p.hasPermission("bit.replaceitems.blockplace"))
+            return;
         if (p.getGameMode() == GameMode.CREATIVE && !Config.REPLACE_ITEMS_IN_CREATIVE.getBoolean()) return;
         Block placed = e.getBlockPlaced();
         ItemStack item = new ItemStack(placed.getType(), 1);
@@ -27,6 +28,9 @@ public class BlockPlaceListener implements Listener {
         Utils.debug(e.getClass().getName() + " was fired! " + p.getName() + " Placed " + item);
 
         ItemStack held = inv.getItem(inv.getHeldItemSlot());
+        if (!held.getType().isBlock()) return;
+
+
         Utils.debug("Item held before actually removing: " + held);
         if (held.getAmount() == 1) {
             held = null;
