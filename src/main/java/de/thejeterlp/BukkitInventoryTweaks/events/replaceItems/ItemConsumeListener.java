6dc9a0204bd4188d13ff2290ff37be5346623871
TheJeterLP <joey.peter1998@gmail.com>
2022-12-25 13:18:25 +0100

    start fix of #23

--- src/main/java/de/thejeterlp/BukkitInventoryTweaks/events/replaceItems/ItemConsumeListener.java
+++ src/main/java/de/thejeterlp/BukkitInventoryTweaks/events/replaceItems/ItemConsumeListener.java
@@ -17,7 +17,8 @@ public class ItemConsumeListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onItemConsume(PlayerItemConsumeEvent e) {
         Player p = e.getPlayer();
-        if(e.isCancelled() || !Config.REPLACE_ITEMS_ON_CONSUME.getBoolean() || !p.hasPermission("bit.replaceitems.itemconsumed")) return;
+        if (e.isCancelled() || !Config.REPLACE_ITEMS_ON_CONSUME.getBoolean() || !p.hasPermission("bit.replaceitems.itemconsumed"))
+            return;
         if (p.getGameMode() == GameMode.CREATIVE && !Config.REPLACE_ITEMS_IN_CREATIVE.getBoolean()) return;
         ItemStack item = e.getItem();
         PlayerInventory inv = p.getInventory();
@@ -25,13 +26,21 @@ public class ItemConsumeListener implements Listener {
         Utils.debug(e.getClass().getName() + " was fired! " + p.getName() + " consumed " + item);
 
         if (item.getAmount() > 1) return;
-        inv.setItem(inv.getHeldItemSlot(), new ItemStack(Material.AIR));
+
+        ItemStack consumed = inv.getItem(e.getHand());
+        if (consumed == null) {
+            return;
+        }
+        ItemStack cloneConsumed = consumed.clone();
+        consumed.setType(Material.AIR);
+
         ItemStack target = Utils.getMatchingItemIfExisting(item, inv);
         if (target == null) return;
-        inv.setItem(inv.first(target), new ItemStack(Material.AIR));
-        inv.setItem(inv.getHeldItemSlot(), target);
+        ItemStack copy = target.clone();
+        target.setType(Material.AIR);
+        inv.setItem(e.getHand(), copy);
         Utils.playSound(p);
-        Utils.debug("Matching Item found! Replacing with " + target);
+        Utils.debug("Matching Item found! Replacing " + cloneConsumed + " with " + copy);
     }
 
 }
